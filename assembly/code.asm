2f2a5e6341dd52298e57b780254a01cdac3ebcc5
weixu000 <wei-x15@mails.tsinghua.edu.cn>
2017-07-06 14:38:07 +0800

    外设、汇编稳了

--- assembly/code.asm
+++ assembly/code.asm
@@ -2,43 +2,42 @@ j Initialization
 j Interrupt
 
 Initialization:
-lui $t0,16384                  # $t0=TH地址
-addi $t1,$t0,12                # led地址
-addi $t2,$t0,26                # $t2=digi_in地址
-addi $t3,$t0,24                # TXD地址
-addi $t4,$t0,28                # RXD地址
-addi $t5,$zero,0               # 显示第几位？？？
+lui $t0,16384
+addi $t1,$t0,12
+addi $t2,$t0,20
+addi $t4,$t0,28
+addi $t5,$zero,0
+addi $t9,$zero,0
 lui $t6,65535
-ori $t6,$t6,15535             # 频率1kHz
+ori $t6,$t6,15535
 sw $t6,0($t0)
 lui $t6,65535
 ori $t6,$t6,65535
-sw $t6,4($t0)                  # 设置TL
+sw $t6,4($t0)
 addi $t6,$zero,3
-sw $t6,8($t0)                  # 设置TCON，启动定时器
-addi $ra,$zero,76             # 80是哪？是72的Read嘛？
-sw $t6,8($t0)                  # 使TCON011 ？？
-jr $ra                      # 跳到用户
+sw $t6,8($t0)
+addi $ra,$zero,80
+sw $t6,8($t0)
+jr $ra
 
 Read:
-lw $t6,32($t0)                 # 读UARTCON
+lw $t6,32($t0)
 srl $t6,$t6,3
 addi $t6,$t6,-1
-beqz $t6,Load2                 # 收到数据
+beqz $t6,Load2
 j Read
 Load2:
-beqz $t5,Load1
-addi $t5,$zero,0               # 将t5初始化为0留作interrupt里控制显示第几位？？
-lw $s1,0($t4)                  # $s1第二个数据？
+beqz $t9,Load1
+lw $s1,0($t4)
 j Main
 Load1:
-lw $s0,0($t4)                  # $s0第一个数据？
-addi $t5,$t5,1
+lw $s0,0($t4)
+addi $t9,$t9,1
 j Read
 
 Interrupt:
 addi $t6,$zero,0
-sw $t6,8($t0)                  # TCON=0
+sw $t6,8($t0)
 beqz $t5,Num1
 addi $t6,$t5,-1
 beqz $t6,Num2
@@ -54,7 +53,7 @@ addi $s2,$s2,3584
 sw $s2,0($t2)
 addi $t5,$t5,1
 addi $t6,$zero,3
-sw $t6,8($t0)                  # 设置TCON，启动定时器
+sw $t6,8($t0)
 addi $26, $26, -4
 jr $26
 Num2:
@@ -64,7 +63,7 @@ addi $s2,$s2,3328
 sw $s2,0($t2)
 addi $t5,$t5,1
 addi $t6,$zero,3
-sw $t6,8($t0)                  # 设置TCON，启动定时器
+sw $t6,8($t0)
 addi $26, $26, -4
 jr $26
 Num3:
@@ -75,7 +74,7 @@ addi $s2,$s2,2816
 sw $s2,0($t2)
 addi $t5,$t5,1
 addi $t6,$zero,3
-sw $t6,8($t0)                  # 设置TCON，启动定时器
+sw $t6,8($t0)
 addi $26, $26, -4
 jr $26
 Num4:
@@ -85,56 +84,56 @@ addi $s2,$s2,1792
 sw $s2,0($t2)
 addi $t5,$zero,0
 addi $t6,$zero,3
-sw $t6,8($t0)                  # 设置TCON，启动定时器
+sw $t6,8($t0)
 addi $26, $26, -4
 jr $26
 
 Translate1:
-addi $t7,$zero,2                #8'b0000_0010
+addi $t7,$zero,3
 beqz $s2,Translate2
-addi $t7,$zero,158                #8'b1001_1110
+addi $t7,$zero,159
 addi $t8,$s2,-1
 beqz $t8,Translate2
-addi $t7,$zero,36                #8'b0010_0100
+addi $t7,$zero,37
 addi $t8,$s2,-2
 beqz $t8,Translate2
-addi $t7,$zero,12                #8'b0000_1100
+addi $t7,$zero,13
 addi $t8,$s2,-3
 beqz $t8,Translate2
-addi $t7,$zero,152                #8'b1001_1000
+addi $t7,$zero,153
 addi $t8,$s2,-4
 beqz $t8,Translate2
-addi $t7,$zero,72                #8'b0100_1000
+addi $t7,$zero,73
 addi $t8,$s2,-5
 beqz $t8,Translate2
-addi $t7,$zero,64                #8'b0100_0000
+addi $t7,$zero,65
 addi $t8,$s2,-6
 beqz $t8,Translate2
-addi $t7,$zero,30                #8'b0001_1110
+addi $t7,$zero,31
 addi $t8,$s2,-7
 beqz $t8,Translate2
-addi $t7,$zero,0                #8'b0000_0000
+addi $t7,$zero,1
 addi $t8,$s2,-8
 beqz $t8,Translate2
-addi $t7,$zero,4                #8'b0000_0100
+addi $t7,$zero,9
 addi $t8,$s2,-9
 beqz $t8,Translate2
-addi $t7,$zero,8                #8'b0000_1000
+addi $t7,$zero,17
 addi $t8,$s2,-10
 beqz $t8,Translate2
-addi $t7,$zero,192                #8'b1100_0000
+addi $t7,$zero,193
 addi $t8,$s2,-11
 beqz $t8,Translate2
-addi $t7,$zero,98                #8'b0110_0010
+addi $t7,$zero,99
 addi $t8,$s2,-12
 beqz $t8,Translate2
-addi $t7,$zero,132                #8'b1000_0100
+addi $t7,$zero,133
 addi $t8,$s2,-13
 beqz $t8,Translate2
-addi $t7,$zero,98                #8'b0110_0010
+addi $t7,$zero,97
 addi $t8,$s2,-14
 beqz $t8,Translate2
-addi $t7,$zero,114                #8'b0111_0010
+addi $t7,$zero,113
 addi $t8,$s2,-15
 beqz $t8,Translate2
 Translate2:
@@ -142,25 +141,27 @@ add $s2,$zero,$t7
 jr $ra
 
 Main:
-beq $s0,$s1,Res1               # 相等大公约数是自己
+beq $s0,$s1,Res1 
+addi $s3,$s0,0
+addi $s4,$s1,0             
 Loop1:
-bgt $s0,$s1,Loop2
-sub $t7,$s1,$s0                # s1>s0
-beq $s0,$t7,Res2
-addi $s1,$t7,0
+bgt $s3,$s4,Loop2
+sub $t3,$s4,$s3               
+beq $s3,$t3,Res2
+addi $s4,$t3,0
 j Loop1
 Loop2:
-sub $t7,$s0,$s1                # s0>s1
-beq $s1,$t7,Res2
-addi $s0,$t7,0
+sub $t3,$s3,$s4               
+beq $s4,$t3,Res2
+addi $s3,$t3,0
 j Loop1
 Res1:
-addi $a0,$s0,0
-addi $t7,$a0,0
+addi $a0,$s3,0
+addi $t3,$a0,0
 Res2:
-addi $a0,$t7,0
-sw $a0,0($t1)                  # led显示结果
-sw $a0,0($t3)                  # 写入TXD
+addi $a0,$t3,0
+sw $a0,0($t1)                  
+sw $a0,24($t0)                  
 
 Halt:
-beq	$zero, $zero, Halt         # 死循环
+beq	$zero, $zero, Halt         
